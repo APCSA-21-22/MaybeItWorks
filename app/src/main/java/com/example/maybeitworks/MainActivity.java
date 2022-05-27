d75bfca00e4b98fcb2a6011f6ab82a3a81a205b5
Ovi1001 <ovi26470@gmail.com>
2022-05-27 08:35:35 -0700

    Variable hi added to Main Activity in JesusBranch to keep track of an arbitrary number

--- app/src/main/java/com/example/maybeitworks/MainActivity.java
+++ app/src/main/java/com/example/maybeitworks/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int hi = 7; // the amount of times that hi was said today
     }
 }
