04453c70580b3a7deaa503eb5938847ba7e403c4
Jonathan <steelsouljs@gmail.com>
2022-05-27 08:34:28 -0700

    Comments added to MainActivity.java in JonathanBranch

--- app/src/main/java/com/example/maybeitworks/MainActivity.java
+++ app/src/main/java/com/example/maybeitworks/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }//I made a branch of MaybeItWorks
+    //Why is it called an oven when you oven the cold food and ovout the hot food.
 }
